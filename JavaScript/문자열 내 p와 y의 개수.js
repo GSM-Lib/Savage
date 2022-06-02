9d35601a1d38f57eed39db0adb97a037ced16460
Huuuunee <s21024@gsm.hs.kr>
2022-06-02 11:19:33 +0900

    :sparkles: 혁신\

--- JavaScript/문자열 내 p와 y의 개수.js
+++ JavaScript/문자열 내 p와 y의 개수.js
@@ -1,15 +1,5 @@
 function solution(s) {
-  const p = /p/i;
-  const y = /y/i;
-  let answer = true;
-  let countP = 0;
-  let countY = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (p.test(s[i])) {
-      countP += 1;
-    } else if (y.test(s[i])) {
-      countY += 1;
-    }
-  }
-  return countP === countY ? (answer = true) : (answer = false);
+  const p = /p/gi;
+  const y = /y/gi;
+  return s.match(p).length === s.match(y).length;
 }
