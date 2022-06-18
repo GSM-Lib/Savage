b01ec6ce1510bc45493053991297ffe3ff19a70e
시훈 <81404026+Huuuunee@users.noreply.github.com>
2022-06-19 01:51:16 +0900

    Update 맵(카카오).js

--- JavaScript/맵(카카오).js
+++ JavaScript/맵(카카오).js
@@ -1,23 +1,17 @@
-function solution(n, arr1, arr2) {
-  let answer = [];
-  let result = "";
-  let rrrr = [];
-  for (let a = 0; a < n; a++) {
-    arr1[a] = arr1[a].toString(2).padStart(n, "0");
-    arr2[a] = arr2[a].toString(2).padStart(n, "0");
-    for (let b = 0; b < n; b++) {
-      if (
-        (arr1[a][b] == 1 && arr2[a][b] == 1) ||
-        (arr1[a][b] == 1 && arr2[a][b] == 0) ||
-        (arr1[a][b] == 0 && arr2[a][b] == 1)
-      ) {
-        answer[a] += "#";
-      } else if (arr1[a][b] == 0 && arr2[a][b] == 0) answer[a] += " ";
+  function solution(n, arr1, arr2) {
+      let answer = [];
+      let result = "";
+      let rrrr = [];
+      for (let a = 0; a < n; a++) {
+        arr1[a] = arr1[a].toString(2).padStart(n, "0");
+        arr2[a] = arr2[a].toString(2).padStart(n, "0");
+        for (let b = 0; b < n; b++) {
+         arr1[a][b] == 0 && arr2[a][b] == 0 ? answer[a] += " " : answer[a] += "#";
+      }
+      result = answer[a].replace("undefined", "");
+
+      rrrr[a] = result;
     }
-    result = answer[a].replace("undefined", "");
 
-    rrrr[a] = result;
+    return rrrr;
   }
-
-  return rrrr;
-}
