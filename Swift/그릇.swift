ecf148a9d43436c4ed266e9e74de77cac96b5f1f
baegteun <baegteun@gmail.com>
2022-08-04 15:49:11 +0900

    :sparkles: :: 백준 - 그릇

---
+++ Swift/그릇.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let read = readLine()!.map { String($0) }
+var res = 0
+var recent = ""
+for i in read.indices {
+    if i == 0 {
+        res += 10
+        recent = read[i]
+    } else if recent == read[i] {
+        res += 5
+    } else {
+        res += 10
+        recent = read[i]
+    }
+}
+
+print(res)
+
