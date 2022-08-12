e716890a694ff35bcc4cd07324530075857ed43e
Huuuunee <s21024@gsm.hs.kr>
2022-08-12 13:18:36 +0900

    :recycle: 필요없는 코드 삭제

--- JavaScript/K번째수.js
+++ JavaScript/K번째수.js
@@ -1,9 +1,5 @@
 function solution(array, commands) {
-  let answer = [];
-  commands.forEach((data, index) => {
-    answer[index] = array.slice(data[0] - 1, data[1]).sort((a, b) => a - b)[
-      data[2] - 1
-    ];
+  return commands.map((data) => {
+    return array.slice(data[0] - 1, data[1]).sort((a, b) => a - b)[data[2] - 1];
   });
-  return answer;
 }
