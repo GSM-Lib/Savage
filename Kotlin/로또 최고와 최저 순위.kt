6e9b379f6225054c42f1ac910b474a5341cfbd43
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-08-14 00:51:51 +0900

    Delete 로또 최고와 최저 순위.kt

--- Kotlin/로또 최고와 최저 순위.kt
+++
@@ -1,2 +0,0 @@
-class Solution
-    fun solution(lottos: IntArray, win_nums: IntArray) = intArrayOf(7 - (lottos.filter { win_nums.contains(it) || it == 0 }.size), 7 - (lottos.filter {win_nums.contains(it)}.size)).map { if (it > 6) 6 else it }.toIntArray()
