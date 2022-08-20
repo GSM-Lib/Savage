5c090e3f99a9dee1c2c1776b80837f62b5d7e309
HumanIearning <kimdh3896@naver.com>
2022-08-21 01:26:41 +0900

    :sparkles: :: 프로그래머스 - 성격 유형 검사하기

---
+++ Python/성격_유형_검사하기.py
@@ -0,0 +1,6 @@
+def solution(survey, choices):
+    result = dict(zip(["RT","CF","JM",'AN'], [0,0,0,0]))
+    score = [[''.join(sorted(s)),-c] if s not in result else [s,c] for s,c in zip(survey,list(map(lambda x: x-4, choices)))]
+    for s in score:
+        result[s[0]] += s[1]
+    return ''.join([r[0][0] if r[1] <= 0 else r[0][1] for r in result.items()])
