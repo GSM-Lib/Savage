527c243a628e36c5511ddc0d668490148a0303a4
khs3994 <z01039948003@gmail.com>
2022-11-05 22:22:33 +0900

    정렬 합치기

--- Kotlin/나누어_떨어지는_배열.kt
+++ Kotlin/나누어_떨어지는_배열.kt
@@ -1,5 +1,4 @@
-fun solution2(arr: IntArray, divisor: Int): IntArray {
-    arr.sort()
-    val answer: IntArray = arr.filter { it % divisor == 0 }.toIntArray()
+fun solution(arr: IntArray, divisor: Int): IntArray {
+    val answer: IntArray = arr.filter { it % divisor == 0 }.sorted().toIntArray()
     return if (answer.isEmpty()) intArrayOf(-1) else answer
 }
