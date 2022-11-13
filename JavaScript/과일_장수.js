5207190a1aa042d180296fb3378a001e821d69ca
Huuuunee <s21024@gsm.hs.kr>
2022-11-13 18:39:27 +0900

    :sparkles: 과일 장수

---
+++ JavaScript/과일_장수.js
@@ -0,0 +1,14 @@
+function solution(k, m, score) {
+  let answer = 0;
+  score.sort((a, b) => {
+    return b - a;
+  });
+  let idx = m - 1;
+  for (let i = 0; i < Math.floor(score.length / m); i++) {
+    answer += score[idx] * m;
+    idx += m;
+  }
+  return answer;
+}
+
+console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
