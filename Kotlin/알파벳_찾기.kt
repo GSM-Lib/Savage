6fccea0d08343e445c154b475206b1c181242ec7
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-11-15 08:40:19 +0900

    ✨ :: 백준 - 알파벳 찾기

--- Kotlin/알파벳_찾기.kt
+++ Kotlin/알파벳_찾기.kt
@@ -3,6 +3,6 @@ import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val arr = readLine().toCharArray()
-    val list = (97..122).toList().map { it.toChar() }.map { arr.indexOf(it) }
+    val list = (97..122).toList().map { arr.indexOf(it.toChar()) }
     print(list.joinToString(" "))
 }
