01d81a127d6c573a31fbfa9ac3ce2150fc943b8a
Huuuunee <s21024@gsm.hs.kr>
2022-11-14 18:18:24 +0900

    :sparkles: 최빈값_구하기

---
+++ JavaScript/최빈값_구하기.js
@@ -0,0 +1,12 @@
+function solution(array) {
+  let setArr = [...new Set(array)]; // 1 2 3 4
+  let cnt = [];
+  for (let i = 0; i < setArr.length; i++) {
+    cnt.push(array.filter((result) => result === setArr[i]).length);
+  }
+  let maxCnt = Math.max(...cnt);
+  return cnt.filter((result) => result === maxCnt).length > 1
+    ? -1
+    : setArr[cnt.indexOf(maxCnt)];
+}
+console.log(solution([1]));
