1a08b27f808e6a6f8bed64ed97e1e225de1468f7
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-04 22:38:19 +0900

    :recycle: class제거
    ㅇㅇ

--- Kotlin/서울에서_김서방_찾기.kt
+++ Kotlin/서울에서_김서방_찾기.kt
@@ -1,3 +1,1 @@
-class Solution {
-    fun solution(seoul: Array<String>): String = "김서방은 ${seoul.indexOf("Kim")}에 있다"
-}
+fun solution(seoul: Array<String>): String = "김서방은 ${seoul.indexOf("Kim")}에 있다"
