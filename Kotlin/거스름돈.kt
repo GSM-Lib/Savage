346202d156d160e5f0cf83455b03b3ac4169f3a7
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-01 17:49:20 +0900

    ♻️ :: 아잇 배열 왜쓴거야

--- Kotlin/거스름돈.kt
+++ Kotlin/거스름돈.kt
@@ -7,15 +7,13 @@ fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in
         println(-1)
         return@with
     }
-    val coinList = listOf(5, 2)
     var coin = 0
     while (n >= 5) {
         coin++
-        n -= coinList[0]
+        n -= 5
     }
     coin += if (n == 3 || n == 1) (n + 5) / 2 - 1
         else (n / 2)
     println(coin)
 
 }
-
